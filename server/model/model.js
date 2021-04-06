66979208f8c3fdd9cf2031af7bcafe6d315dd6e6
Soumodeep Ganguly <soumodeep.ganguly.007@gmail.com>
2021-04-06 10:10:05 +0530

    First Commit

---
+++ server/model/model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+var schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    date: {
+        type: String,
+
+    },
+    mobile: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    rating: {
+        type: String,
+
+    },
+    wallet: {
+        type: String,
+
+    },
+
+    status: String
+})
+
+const Userdb = mongoose.model('origano', schema);
+module.exports = Userdb;
